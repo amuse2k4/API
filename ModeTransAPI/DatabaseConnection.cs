3b7b1667ab32247849787d923b19602f0ba8757f
AMUSE <AMUSE@L-PC0Q9PC6>
2018-08-29 11:06:49 -0500

    Add project files.

---
+++ ModeTransAPI/DatabaseConnection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace ModeTransAPI
+{
+    public class DatabaseConnection
+    { 
+
+        public DatabaseConnection()
+        {
+            SqlConnection myConnection = new SqlConnection();
+
+            SqlConnectionStringBuilder myBuilder = new SqlConnectionStringBuilder();
+
+            myBuilder.UserID = "amuse";
+
+            myBuilder.Password = "Washington2";
+
+            myBuilder.DataSource = "master";
+
+            myConnection.ConnectionString = myBuilder.ConnectionString;
+        }
+        
+        
+    }
+}
